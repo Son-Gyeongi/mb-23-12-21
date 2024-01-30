9f455bed62b9ffc1edfe52d3c579efe5c185e19c
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-30 11:52:54 +0900

    23 12 26, p 13296, 41강, order.isPayable 함수를 둬서 결제가 가능한 상황인 경우에만 결제폼 노출

--- src/main/java/com/ll/mb/domain/product/order/entity/Order.java
+++ src/main/java/com/ll/mb/domain/product/order/entity/Order.java
@@ -94,4 +94,12 @@ public class Order extends BaseEntity { // 주문 1개
                 + (AppConfig.isNotProd() ? "-test-" + UUID.randomUUID().toString() : "")
                 + "__" + getId();
     }
+
+    // 결제 가능한 상태 확인
+    public boolean isPayable() {
+        if (payDate != null) return false; // 결제일이 null이 아닐경우 false
+        if (cancelDate != null) return false; // 취소일이 null이 아닐경우 false
+
+        return true; // 결제가 가능하다.
+    }
 }
